e50058ef38d258b3f9f031aefe4a2dcf43cd2b5a
wemlion <angusfu1126@qq.com>
2018-08-06 15:48:41 +0800

    commit

---
+++ test/index.vue
@@ -0,0 +1,13 @@
+<script>
+// wrong format
+// just for manual test
+export default {
+    data()
+    {
+        return {a: 22}
+    },
+    x(params) {
+        console.log({a: 22 });
+    }
+};
+</script>
